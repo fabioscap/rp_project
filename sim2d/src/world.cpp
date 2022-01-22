d8e8512503418707648001368b42eaf5efb81abd
fabioscap <fabiovt_99@hotmail.it>
2022-01-22 17:54:13 +0100

    added footprint for the robot

--- sim2d/src/world.cpp
+++ sim2d/src/world.cpp
@@ -1,4 +1,5 @@
 #include "world.h"
+#include <math.h>
 
 using namespace sim2d;
 
@@ -8,7 +9,22 @@ void World::update(){
 
 }
 bool World::check_collision() {
+    
 
-    return false;
+}
 
+std::vector<pos2> World::get_footprint() {
+    std::vector<pos2> vec;
+    int r2 = radius*radius;
+    for(int x =-radius; x<=radius; ++x) {
+        for(int y =-radius; y<=radius; ++y) {
+            int d = (x)*(x)+(y)*(y);
+            std::cout << d<< std::endl;
+            if (d<=radius*radius+1) {
+                // point belongs to robot
+                vec.push_back(pos2{{x,y}});
+            }
+        }
+    }
+    return vec;
 }
