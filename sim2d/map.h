52fbd9ab731c6afc44fa2a7974a9b7db2ce244ce
fabioscap <fabiovt_99@hotmail.it>
2022-01-22 17:53:08 +0100

    added a typedef

--- sim2d/map.h
+++ sim2d/map.h
@@ -1,25 +1,26 @@
 #pragma once
 
 #include <iostream>
-
 #define WALL 100
+#define OTHER 50
 
-
+typedef int32_t cell_index;
 
 namespace sim2d {
 // maybe template the dimensions
+
 class Map {
     public:
-    const float resolution; // m/pixel
-    const uint32_t width;  // pixels
-    const uint32_t height; // pixels
-    const uint32_t dimension; // pixels
+    const double resolution; // m/pixel
+    const cell_index width;  // pixels
+    const cell_index height; // pixels
+    const cell_index dimension; // pixels
     int8_t* data; // occupancy grid
 
-    inline int8_t& at(int i, int j) {return data[i*height+j];}
-    inline const int8_t& at(int i, int j) const {return data[i*height+j];}
+    inline int8_t& at(cell_index i, cell_index j) {return data[i*height+j];}
+    inline const int8_t& at(cell_index i, cell_index j) const {return data[i*height+j];}
 
-    Map(float res, uint32_t w, uint32_t h, int8_t* d): resolution(res), 
+    Map(double res, cell_index w, cell_index h, int8_t* d): resolution(res), 
                                             width(w),
                                             height(h),
                                             data(d),
@@ -31,12 +32,12 @@ std::ostream&  operator << (std::ostream& os, const Map& m) {
     for (int i=0; i<m.width; ++i) { 
         for (int j=0; j<m.height; ++j) {
             if (m.at(i,j) == WALL) os << "* ";
+            else if (m.at(i,j) == OTHER) os << "o ";
             else os << "  ";
         }
         os << std::endl;
     }
     return os;
 }
-
 
 }
